c52324b947b8f87332c4de6898c493621d886d8f
Andrés Díaz <andresdiazfigueroa@outlook.com>
2024-03-21 00:31:18 -0500

    se realiza commit al proyecto con instalacion dependecias React y ajustes  a SRC

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
 import './App.css';
-import Navigation from './components/navigation';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      < h2 > <Navigation></Navigation></h2>
+      <h1> hola </h1> 
+      <h1> buenas </h1> 
     </div>
   );
 }
