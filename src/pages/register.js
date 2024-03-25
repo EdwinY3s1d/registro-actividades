e4f0f59fa63fa25e582afcaeac989f1f2c678c17
Andrés Díaz <andresdiazfigueroa@outlook.com>
2024-03-25 16:47:07 -0500

    Se validan confilcos con cambios en main remoto y se incluye  elobjeto formData para las variables del formulario

--- src/pages/register.js
+++ src/pages/register.js
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-import React, { useState } from 'react';
-import '../index.css'
-
-
-function Register() {
-    const [actividad, setActividad] = useState('');
-    const [horaInicio, setHoraInicio] = useState('');
-    const [horaFin, setHoraFin] = useState('');
-    const [numeroIdentificacion, setNumeroIdentificacion] = useState('');
-=======
 import React, { useState } from "react";
 import './register.css';
 
@@ -20,80 +9,29 @@ function Register() {
     });
 
     const handleChange = (e) => {
-        setFormData({...formData, [e.target.name]: e.target.value});
+        setFormData({ ...formData, [e.target.name]: e.target.value });
     }
->>>>>>> a7b57fa4b67c6829bb5b0f7617026b67fc8e96ce
 
     const handleSubmit = (e) => {
         e.preventDefault();
         // Aquí puedes enviar los datos al servidor o realizar otras acciones
-        console.log('Número de identificación:', numeroIdentificacion);
-        console.log('Actividad:', actividad);
-        console.log('Hora de inicio:', horaInicio);
-        console.log('Hora de fin:', horaFin);
+        console.log('Número de identificación:', formData.numeroIdentificacion);
+        console.log('Actividad:',  formData.actividad);
+        console.log('Hora de inicio:', formData.horaInicio);
+        console.log('Hora de fin:', formData.horaFin);
     };
 
     return (
-<<<<<<< HEAD
-        <div className="container">
-            <h1>Registro de Actividades</h1>
-            <form onSubmit={handleSubmit}>
-                <div className="form-group">
-                    <label htmlFor="numeroIdentificacion">Número de identificación</label>
-                    <input
-                        type="text"
-                        id="numeroIdentificacion"
-                        className="form-control"
-                        value={numeroIdentificacion}
-                        onChange={(e) => setNumeroIdentificacion(e.target.value)}
-                    />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="actividad">Actividad:</label>
-                    <input
-                        type="text"
-                        id="actividad"
-                        className="form-control"
-                        value={actividad}
-                        onChange={(e) => setActividad(e.target.value)}
-                    />
-                </div>
-                <div className="form-row">
-                    <div className="form-group col">
-                        <label htmlFor="horaInicio">Hora de inicio:</label>
-                        <input
-                            type="time"
-                            id="horaInicio"
-                            className="form-control"
-                            value={horaInicio}
-                            onChange={(e) => setHoraInicio(e.target.value)}
-                        />
-                    </div>
-                    <div className="form-group col">
-                        <label htmlFor="horaFin">Hora de fin:</label>
-                        <input
-                            type="time"
-                            id="horaFin"
-                            className="form-control"
-                            value={horaFin}
-                            onChange={(e) => setHoraFin(e.target.value)}
-                        />
-                    </div>
-                </div>
-                <button type="submit" className="btn btn-primary">
-                    Registrar
-                </button>
-=======
         <div class="cover-container-register d-flex p-3 mx-auto flex-column">
             <form onSubmit={handleSubmit} class="form-register">
                 <h1>Registrar actividad</h1>
-                
+
                 <div class="col-md-12 mb-4">
                     <div class="row">
                         <div class="col-md-12 mb-6">
                             <label class="form-label col-12">
                                 Actividad:
-                                <input type="text" name="activity" onChange={handleChange} className="form-control"/>
+                                <input type="text" name="activity" onChange={handleChange} className="form-control" />
                             </label>
                         </div>
 
@@ -105,8 +43,7 @@ function Register() {
                         </div>
                         <button type="submit" className="button-register">Enviar</button>
                     </div>
-                </div> 
->>>>>>> a7b57fa4b67c6829bb5b0f7617026b67fc8e96ce
+                </div>
             </form>
         </div>
     );
